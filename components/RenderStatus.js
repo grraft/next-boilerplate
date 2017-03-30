d21a9f00eb6771cae3eddd66ad44c38e6b8eadcd
grraft <kunal.mandalia@grraft.com>
2017-03-30 22:25:53 +0100

    Remove style jsx and place in global for ssr

--- components/RenderStatus.js
+++ components/RenderStatus.js
@@ -2,36 +2,13 @@ import React from 'react'
 
 const RenderStatus = ({isServer}) => {
   return (
-    <div className='_container align--center'>
-        <div id='prefix' className='_box'>rendered on </div>
-        <div id='suffix' className={'_box ' + (isServer ? '_box--green' : '_box--blue')}>
-          {isServer ? 'server' : 'client'}
-        </div>
+    <div className='component__render-status_container align--center'>
+      <div className='component__render-status--prefix component__render-status--box'>rendered on </div>
+      <div className={'component__render-status--suffix component__render-status--box ' + (isServer ? 'component__render-status--box-green' : 'component__render-status--box-blue')}>
+        {isServer ? 'server' : 'client'}
+      </div>
       <style jsx>{`
-        ._container {
-          display: inline-flex;
-          font-size: small;
-          margin-right: 4px;
-        }
-        #prefix {
-          padding: 2px 4px;          
-        }
-        #suffix {
-          color: white;
-          font-weight: 800;
-          padding: 2px 4px;
-        }
-        ._box {
-          border-width: thin;
-          border-color: grey;
-          border-style: solid;
-        }
-        ._box--green {
-          background-color: green;
-        }
-        ._box--blue {
-          background-color: dodgerblue;
-        }
+        
         `}</style>
     </div>
   )
