14c77911c67488f5cde2d3bfb931c26e3097dbc6
grraft <kunal.mandalia@grraft.com>
2017-03-30 22:22:53 +0100

    Add fontawesome and bootstrap css

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,10 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel='shortcut icon' type='image/x-icon' href='/static/assets/favicon.ico' />
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <link rel='shortcut icon' type='image/x-icon' href='/static/assets/favicon.ico' />
+          <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' integrity='sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN' crossOrigin='anonymous' />
+          <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossOrigin='anonymous' />
           <title>Next Boilerplate</title>
         </Head>
         <body>
