39481fce13effb95226cfb629bba63e2981d2ffd
grraft <kunal.mandalia@grraft.com>
2017-03-31 14:54:32 +0100

    Start on env port or 3000

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const PORT = process.env.PORT || 3000
 
 app.prepare().then(() => {
   createServer((req, res) => {
@@ -20,8 +21,8 @@ app.prepare().then(() => {
       handle(req, res, parsedUrl)
     }
   })
-  .listen(3000, (err) => {
+  .listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Server Started on ${PORT}`)
   })
 })
